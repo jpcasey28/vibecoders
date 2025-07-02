c57bc06309999efad031637b4c1ea173578cf75d
jpcasey28 <jpcasey28@gmail.com>
2025-07-02 14:06:51 -0400

    bug fixe3

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,20 +1,18 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies, ReadonlyRequestCookies } from 'next/headers'
+import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore: ReadonlyRequestCookies = cookies()
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return cookies().get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookies().set({ name, value, ...options })
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -23,7 +21,7 @@ export function createClient() {
         },
         remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.delete({ name, ...options })
+            cookies().delete({ name, ...options })
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
